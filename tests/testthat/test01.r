7c17ed2eff76567f91914e71e4e97b0e8df19f73
wada <wada@kw2024>
2024-06-11 23:28:30 +0900

    Data included

--- tests/testthat/test01.r
+++ tests/testthat/test01.r
@@ -1,9 +1,10 @@
 # download wine dataset from UCI Machine Learning Repository 
 # 174 recordes, 14 variables
 
-tmp <- tempfile()
-download.file("http://archive.ics.uci.edu/static/public/109/wine.zip", tmp)
-wine <- read.csv(unz(tmp, "wine.data"), header=F)
+# tmp <- tempfile()
+# download.file("http://archive.ics.uci.edu/static/public/109/wine.zip", tmp)
+# wine <- read.csv(unz(tmp, "wine.data"), header=F)
+data(wine)
 
 o1 <- RMSDp(wine[,-1], cores=2)
 sum(o1$ot-1) # number of outliers
