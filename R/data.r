d834c733b5b9a94c3712a1a0d370d6a391032e8e
kazwd2008 <kazwd2008@gmail.com>
2024-06-11 23:32:55 +0900

    Data included

--- R/data.r
+++ R/data.r
@@ -3,22 +3,22 @@
 #' A subset of data from the World Health Organization Global Tuberculosis
 #' Report ...
 #'
-#' @format 
-#' A data frame with 178 rows and 13 columns. These data are the results of a chemical analysis of wines grown in the same region in Italy but derived from three different cultivars. The analysis determined the quantities of 13 constituents found in each of the three types of wines.  # nolint: line_length_linter.
-#' 
+#' @format ## `wine`
+#' A data frame with 178 rows and 13 columns:
 #' \describe{
-#'   \item{Alcohol}
-#'   \item{Malic acid}
-#'   \item{Ash}
-#'   \item{Alcalinity of ash}
-#'   \item{Magnesium}
-#'   \item{Total phenols}
-#'   \item{Flavonoids}
-#'   \item{Nonflavanoid phenols}
-#'   \item{Proanthocyanins}
-#'   \item{Color intensity}
-#'   \item{Hue}
-#'   \item{OD280/OD315 of diluted wines}
-#'   \item{Proline}
+#'   \item{Alcohol}{Alcohol}
+#'   \item{Malic acid}{Malic acid}
+#'   \item{Ash}{Ash}
+#'   \item{Alcalinity of ash}{Alcalinity of ash}
+#'   \item{Magnesium}{Magnesium}
+#'   \item{Total phenols}{Total phenols}
+#'   \item{Flavonoids}{Flavonoids}
+#'   \item{Nonflavanoid phenols}{Nonflavanoid phenols}
+#'   \item{Proanthocyanins}{Proanthocyanins}
+#'   \item{Color intensity}{Color intensity}
+#'   \item{Hue}{Hue}
+#'   \item{OD280/OD315 of diluted wines}{OD280/OD315 of diluted wines}
+#'   \item{Proline}{Proline}
 #' }
 #' @source <https://archive.ics.uci.edu/dataset/109/wine>
+"wine"
